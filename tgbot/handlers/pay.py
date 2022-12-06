f6af2ffbdb16a6de30b0be8c772955dad04cb609
cmdrBebop <kostyadevyatov@gmail.com>
2022-12-06 19:22:39 +0300

    Add robokassa(not working)

--- tgbot/handlers/pay.py
+++ tgbot/handlers/pay.py
@@ -1,11 +1,27 @@
 from aiogram import Dispatcher
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, LabeledPrice
 
 from tgbot.misc import callbacks
 
 
-async def show_pay_menu(call: CallbackQuery):
-    await call.answer('Функционал пока недоступен!', show_alert=True)
+async def show_pay_menu(call: CallbackQuery, callback_data: dict):
+    config = call.bot.get('config')
+    price = callback_data['price']
+
+    product = LabeledPrice(label='Настоящая Машина Времени', amount=4200000)
+    print(config.robokassa.token)
+    await call.bot.send_invoice(
+        call.message.chat.id,
+        title='Тест',
+        description='Описание',
+        provider_token=config.robokassa.token,
+        currency='rub',
+        prices=[product],
+        start_parameter='time-machine-example',
+        payload='some-invoice-payload-for-our-internal-use'
+    )
+    # await call.answer('Функционал пока недоступен!', show_alert=True)
+    await call.answer()
 
 
 def register_pay(dp: Dispatcher):
